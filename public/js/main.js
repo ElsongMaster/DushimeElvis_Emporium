42017b2b67814545bdaaee6682080502a7be40e6
ElsongMaster <elsongmaster@gmail.com>
2021-06-16 16:37:59 +0200

    Mode nuit

--- public/js/main.js
+++ public/js/main.js
@@ -24,21 +24,48 @@ window.addEventListener("resize", () => {
 //NavBar fixed au scroll
 
 let navNode;
+let NavBar_Node = document.querySelector(".NavBar");
 let container_NavBar = document.querySelector(".container_NavBar");
 document.addEventListener("scroll", () => {
   navNode = document.querySelector("nav");
   // console.log(window.pageYOffset);
   if (window.pageYOffset == 0) {
-    console.log("dans ma condition");
     container_NavBar.classList.remove("display_flex");
     navNode.classList.remove("position_fixed");
+    NavBar_Node.classList.remove("width_half");
   } else {
     if (
       !Array.from(navNode.classList).includes("position_fixed") &&
-      !Array.from(container_NavBar.classList).includes("display_flex")
+      !Array.from(container_NavBar.classList).includes("display_flex") &&
+      !Array.from(NavBar_Node.classList).includes("width_half")
     ) {
       navNode.classList.add("position_fixed");
       container_NavBar.classList.add("display_flex");
+      NavBar_Node.classList.add("width_half");
     }
   }
+});
+
+//mode nuit
+
+let btn_DayMode = document.querySelector(".day_mode");
+let btn_NightMode = document.querySelector(".night_mode");
+
+btn_DayMode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "white";
+  navNode.style.backgroundColor = "white";
+
+  container_NavBar.classList.remove("color_white");
+  Array.from(container_NavBar.querySelectorAll("li>a")).forEach((elt) => {
+    elt.classList.remove("color_white");
+  });
+});
+
+btn_NightMode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "black";
+  navNode.style.backgroundColor = "black";
+  container_NavBar.classList.add("color_white");
+  Array.from(container_NavBar.querySelectorAll("li>a")).forEach((elt) => {
+    elt.classList.add("color_white");
+  });
 });
